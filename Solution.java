8d194b7171282932602167ad532311d8a12bd5bf
sdim-ua <ingersol@yandex.ua>
2019-01-24 23:27:48 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,16 @@
+/*
+    Рисуем линии
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        String str = "8";
+        for (int i =0; i < 10; i++){
+            System.out.print(str);
+        }
+        for (int i =0; i < 10; i++){
+            System.out.println(str);
+        }
+    }
+}
